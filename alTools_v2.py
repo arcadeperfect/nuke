45fe872bb242efea7ed2d6ca08cceda65828bef0
unknown <aharding@NYMVFX001.PSYOP.TV>
2017-08-08 11:59:26 -0400

    First commit

---
+++ alTools_v2.py
@@ -0,0 +1,14 @@
+def hide_viewer_lines():
+    for i in nuke.allNodes('Viewer'):
+        i['hide_input'].setValue(True)
+
+nuke.menu('Nuke').addCommand('harding/Hide Viewer Lines', hide_viewer_lines, 'ctrl+shift+h')
+
+def harding_backdrop():
+    bc = 1079406079
+    b = nukescripts.autoBackdrop()
+    b['note_font_size'].setValue(40)
+    b['tile_color'].setValue(bc)
+    b['label'].setValue(nuke.getInput('Backdrop Label'))
+
+nuke.menu('Nuke').addCommand('harding/Harding Backdrop', harding_backdrop, 'z')
